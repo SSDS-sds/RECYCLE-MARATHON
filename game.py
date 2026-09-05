7cf2bd188edea556b4cd780316aeae0c57d8269e
SSDS-sds <warwicklee1@gmail.com>
2026-09-05 18:58:32 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,10 @@
+import pygame, random, time
+from pygame.locals import *
+
+pygame.init()
+WIDTH = 900
+HEIGHT = 700
+CENTRE_X = WIDTH/2
+CENTRE_Y = HEIGHT/2
+
+pygame.display.set_caption("RECYCLE MARATHON")
